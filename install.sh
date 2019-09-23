77b9e6c1c737eeedb92c01f2a3093f38721f9d91
haingn <52133074+haingn@users.noreply.github.com>
2019-09-23 20:44:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ if [ -z "$1" ]; then
     echo "./install.sh <your_authtoken>"
     exit 1
 fi
-apt-get update && apt-get upgrade -y
+apt-get update -y && sudo apt-get upgrade -y && sudo apt autoremove -y
 apt install libc-ares2 libcrypto++6 libpcrecpp0v5 libmediainfo0v5 libzen0v5 -y
 apt --fix-broken install -y
 wget https://mega.nz/linux/MEGAsync/Raspbian_10.0/arm/megacmd-Raspbian_10.0_armhf.deb
 dpkg -i megacmd-Raspbian_10.0_armhf.deb
-touch /etc/profile.d/startup.sh && echo'mega-sync'>>/etc/profile.d/startup.sh
-sed -i "s/<add_your_token_here>/$1/g" mega-login
+#touch /etc/profile.d/startup.sh && echo'mega-sync'>>/etc/profile.d/startup.sh
+#sed -i "s/<add_your_token_here>/$1/g" mega-login
 cd ~/
