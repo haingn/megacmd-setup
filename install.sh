232a0a9e0280fcd78f64dece4b6b598f678266a0
haingn <52133074+haingn@users.noreply.github.com>
2019-09-23 20:48:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [ -z "$1" ]; then
     echo "./install.sh <your_authtoken>"
     exit 1
 fi
-apt-get update -y && sudo apt-get upgrade -y && sudo apt autoremove -y
+apt-get update -y && apt-get upgrade -y && apt autoremove -y
 apt install libc-ares2 libcrypto++6 libpcrecpp0v5 libmediainfo0v5 libzen0v5 -y
 apt --fix-broken install -y
 wget https://mega.nz/linux/MEGAsync/Raspbian_10.0/arm/megacmd-Raspbian_10.0_armhf.deb
